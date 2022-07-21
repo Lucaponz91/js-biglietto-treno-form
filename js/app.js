ddda18ddf661d79b7748f01a45c0f44afb4db125
Lucaponz91 <lucapo1991@msn.com>
2022-07-21 17:55:53 +0200

    remove comments

--- js/app.js
+++ js/app.js
@@ -3,23 +3,23 @@
 // const inputDist = document.querySelector('input[name="distanza"]');
 // console.log(inputDist)
 const buttonElement = document.querySelector('input[type="submit"]');
-console.log(buttonElement)
+// console.log(buttonElement)
 buttonElement.addEventListener('click', function () {
     const inputName = document.querySelector('input[name="nome_utente"]')
-    console.log(inputName) 
+    // console.log(inputName) 
     const userName = inputName.value
-    console.log(userName)
+    // console.log(userName)
     const name = document.getElementById('nome')
     name.innerHTML = userName
     const inputDist = document.querySelector('input[name="distanza"]');
-    console.log(inputDist)
+    // console.log(inputDist)
     const prezzoBase = parseFloat(inputDist.value) * 0.21
     if (isNaN(prezzoBase)){
         alert('Inserisci un NUMERO di km')
     }
-    console.log(prezzoBase)
+    // console.log(prezzoBase)
     const selectAge = document.querySelector('select[name="age"]');
-    console.log(selectAge)
+    // console.log(selectAge)
     const userAge = document.getElementById('tariffa')
     if (selectAge.value == 0.2 ){
         userAge.innerHTML = 'Young'
@@ -31,7 +31,7 @@ buttonElement.addEventListener('click', function () {
         userAge.innerHTML = 'Adult'
     }
     const discount = parseFloat(selectAge.value)
-    console.log(discount)
+    // console.log(discount)
     const userPrice = document.getElementById('costo')
     const price = (prezzoBase - (prezzoBase * discount))
     const roundedPrice = price.toFixed(2)
