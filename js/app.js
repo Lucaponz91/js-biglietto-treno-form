4057eb243f6af18c7da223a08cd920424d34fc4e
Lucaponz91 <lucapo1991@msn.com>
2022-07-21 16:29:35 +0200

    add eventlistener

---
+++ js/app.js
@@ -0,0 +1,22 @@
+// const inputName = document.querySelector('input[name="nome_utente"]');
+// console.log(inputName) 
+// const inputDist = document.querySelector('input[name="distanza"]');
+// console.log(inputDist)
+const buttonElement = document.querySelector('input[type="submit"]');
+console.log(buttonElement)
+buttonElement.addEventListener('click', function () {
+    const inputName = document.querySelector('input[name="nome_utente"]')
+    console.log(inputName) 
+    const userName = inputName.value
+    console.log(userName)
+    const inputDist = document.querySelector('input[name="distanza"]');
+    console.log(inputDist)
+    const prezzoBase = parseFloat(inputDist.value) * 0.21
+    console.log(prezzoBase)
+    const inputAge = document.querySelector('select[name="age"]');
+    console.log(inputAge)
+    const discount = parseFloat(inputAge.value)
+    console.log(discount)
+// const inputDistance
+}
+)
