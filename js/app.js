20a68e3c1d06ecd91c109c46647b8b4822edfc39
Lucaponz91 <lucapo1991@msn.com>
2022-07-21 17:53:45 +0200

    add ticket(nome,tariffa,costo)

--- js/app.js
+++ js/app.js
@@ -9,6 +9,8 @@ buttonElement.addEventListener('click', function () {
     console.log(inputName) 
     const userName = inputName.value
     console.log(userName)
+    const name = document.getElementById('nome')
+    name.innerHTML = userName
     const inputDist = document.querySelector('input[name="distanza"]');
     console.log(inputDist)
     const prezzoBase = parseFloat(inputDist.value) * 0.21
@@ -16,9 +18,19 @@ buttonElement.addEventListener('click', function () {
         alert('Inserisci un NUMERO di km')
     }
     console.log(prezzoBase)
-    const inputAge = document.querySelector('select[name="age"]');
-    console.log(inputAge)
-    const discount = parseFloat(inputAge.value)
+    const selectAge = document.querySelector('select[name="age"]');
+    console.log(selectAge)
+    const userAge = document.getElementById('tariffa')
+    if (selectAge.value == 0.2 ){
+        userAge.innerHTML = 'Young'
+
+    } else if (selectAge.value == 0.4 ){
+        userAge.innerHTML = 'Senior'
+
+    } else {
+        userAge.innerHTML = 'Adult'
+    }
+    const discount = parseFloat(selectAge.value)
     console.log(discount)
     const userPrice = document.getElementById('costo')
     const price = (prezzoBase - (prezzoBase * discount))
