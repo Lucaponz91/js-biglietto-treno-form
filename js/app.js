611c5db87f487c4c39ccb4fdfe6ea0c85440001e
Lucaponz91 <lucapo1991@msn.com>
2022-07-22 09:28:58 +0200

    random cp, random wagon

--- js/app.js
+++ js/app.js
@@ -1,25 +1,15 @@
-// const inputName = document.querySelector('input[name="nome_utente"]');
-// console.log(inputName) 
-// const inputDist = document.querySelector('input[name="distanza"]');
-// console.log(inputDist)
 const buttonElement = document.querySelector('input[type="submit"]');
-// console.log(buttonElement)
 buttonElement.addEventListener('click', function () {
     const inputName = document.querySelector('input[name="nome_utente"]')
-    // console.log(inputName) 
     const userName = inputName.value
-    // console.log(userName)
     const name = document.getElementById('nome')
     name.innerHTML = userName
     const inputDist = document.querySelector('input[name="distanza"]');
-    // console.log(inputDist)
     const prezzoBase = parseFloat(inputDist.value) * 0.21
     if (isNaN(prezzoBase)){
         alert('Inserisci un NUMERO di km')
     }
-    // console.log(prezzoBase)
     const selectAge = document.querySelector('select[name="age"]');
-    // console.log(selectAge)
     const userAge = document.getElementById('tariffa')
     if (selectAge.value == 0.2 ){
         userAge.innerHTML = 'Young'
@@ -31,10 +21,21 @@ buttonElement.addEventListener('click', function () {
         userAge.innerHTML = 'Adult'
     }
     const discount = parseFloat(selectAge.value)
-    // console.log(discount)
     const userPrice = document.getElementById('costo')
     const price = (prezzoBase - (prezzoBase * discount))
     const roundedPrice = price.toFixed(2)
     userPrice.innerHTML = roundedPrice;
+    // CARROZZA RANDOM
+    // const randomWagon = (function randomInteger(1, 9) {
+    //     return Math.floor(Math.random() * (9 - 1 + 1)) + 1;
+    //   })
+    const randomWagon = Math.floor(Math.random() * 9) + 1
+    const wagon = document.getElementById('carrozza')
+    wagon.innerHTML = randomWagon
+    // CODICE CP RANDOM
+    const randomCP = Math.floor(Math.random() * 99999) + 1
+    const cambioPrenotazione = document.getElementById('cp')
+    cambioPrenotazione.innerHTML = randomCP
+    
 }
 )
